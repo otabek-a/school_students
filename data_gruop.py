cdc2e12a099cdb905b748b6f304460a5faf532d1
otabek-a <otabekabdurasulov245@gmail.com>
2025-02-05 10:07:48 +0500

    ficed

--- data_gruop.py
+++ data_gruop.py
@@ -2,28 +2,36 @@ from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import Update, ReplyKeyboardMarkup
 from tinydb import TinyDB, Query
 import os
-data_group=TinyDB('group.json')
+
+data_group = TinyDB('group.json')
 school = TinyDB('students.json')
 teacher = TinyDB('teachers.json')
+chat = TinyDB('id.json')
 
-chat=TinyDB('id.json')
 Student = Query()
 Teacher = Query()
-result={}
-def create_group(update,context):
-    update.message.reply_text('Pls enter information of group G.group_name,T.teacher_name , D.days_of_course , W.time_of_course. For eaxmple: T.Otabek,D.Monday Friday, W.14-16')
-def clear_group(update,context):
+result = {}
+
+def create_group(update, context):
+    update.message.reply_text("📌 Please enter the group details in this format:\n\n"
+                              "Group.group_name, teacher, days, time ⏳")
+
+def clear_group(update, context):
     data_group.truncate()
-    update.message.reply_text('This list is empty')
+    update.message.reply_text("🗑️ The group list has been cleared!")
+
 def show_group(update, context):
     data = data_group.all()
     if not data:
-        update.message.reply_text("📄 The teacher list is empty. 🏷️")
+        update.message.reply_text("📜 The group list is empty! ❌")
         return
 
-    text = "📚 List of teachers:\n"
+    text = "📚 List of Groups:\n\n"
     for idx, t in enumerate(data, start=1):
-        text += f"{idx}. {t['name']} {t['Surname']} - 📞 {t['Phone']}\n"
+        text += f"{idx}. 📌 {t['group name']}\n"
+        text += f"   👨‍🏫 Teacher: {t['teacher']}\n"
+        text += f"   📅 Days: {t['days']}\n"
+        text += f"   ⏰ Time: {t['time of course']}\n"
+        text += "--------------------------\n"
 
     update.message.reply_text(text)
-    
